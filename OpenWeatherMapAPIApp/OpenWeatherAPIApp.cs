be700b15aecf0cd7954ae8b393c731edf19208d9
Erik Hedman <erikdavid.hedman21@outlook.com>
2023-08-22 11:05:15 -0400

    Created a weather app using OpenWeatherAPI

--- OpenWeatherMapAPIApp/OpenWeatherAPIApp.cs
+++ OpenWeatherMapAPIApp/OpenWeatherAPIApp.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,66 @@ namespace OpenWeatherMapAPIApp
 {
     public class OpenWeatherAPIApp
     {
+        public async void CallOpenWeatherAPI(int zipCode)
+        {
+            var client = new HttpClient();
 
+            var apiKeyObj = File.ReadAllText("appsettings.json");
+
+            
+            var apiKey = JObject.Parse(apiKeyObj).GetValue("apiKey").ToString();
+
+            
+            var apiURL = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&appid={apiKey}&units=imperial";
+
+            var response = client.GetAsync(apiURL).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var responseBody = await response.Content.ReadAsStringAsync();
+                var weatherData = JObject.Parse(responseBody);
+
+                var cityName = weatherData["name"].ToString();
+                var temperature = weatherData["main"]["temp"].ToString();
+                var tempLow = weatherData["main"]["temp_min"].ToString();
+                var tempHigh = weatherData["main"]["temp_max"].ToString();
+                var humidity = weatherData["main"]["humidity"].ToString();
+                var description = weatherData["weather"][0]["description"].ToString();
+                
+
+                Console.WriteLine($"Today in {cityName}");
+                Thread.Sleep(500);
+                Console.Write("~~ ");
+                Thread.Sleep(500);
+                Console.Write("~~ ");
+                Thread.Sleep(500);
+                Console.Write("~~ \n");
+                Thread.Sleep(2000);
+                Console.WriteLine($"The current temperature is: {temperature} degrees Fahrenheit\n\nWith a low today of: {tempLow} degrees Fahrenheit\n\nA high of: {tempHigh} degrees Fahrenheit\n\nAnd humidity at a nice {humidity}%");
+                Thread.Sleep(500);
+                Console.Write("~~ ");
+                Thread.Sleep(500);
+                Console.Write("~~ ");
+                Thread.Sleep(500);
+                Console.Write("~~ \n");
+                Thread.Sleep(2000);
+                Console.WriteLine($"A brief description of the current weather is: {description}\n");
+                Thread.Sleep(500);
+                Console.Write(". . ");
+                Thread.Sleep(500);
+                Console.Write(". . ");
+                Thread.Sleep(500);
+                Console.Write(". . \n\n");
+                Thread.Sleep(2000);
+                Console.WriteLine("And thats it for the weather at the moment.  Come back later for an updated look at your current forecast!");
+                //Console.WriteLine(weatherData);
+            }
+            else
+            {
+               
+                Console.WriteLine($"API request failed with status code: {response.StatusCode}");
+                
+                 
+            }
+        }
     }
 }
--- OpenWeatherMapAPIApp/Program.cs
+++ OpenWeatherMapAPIApp/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using OpenWeatherMapAPIApp;
+
+var weatherAPI = new OpenWeatherAPIApp();
+bool userInputZC = true;
+do
+{
+    Console.WriteLine("Hello, to deterime the weather in your local area, or the area of your choice, please, first enter a zip code here:");
+
+    userInputZC = int.TryParse(Console.ReadLine(), out int zipCode);
+
+
+    if (userInputZC == false)
+    {
+        Console.WriteLine("Invalid entry: Please enter a valid zip code to continue...");
+
+    }
+
+    weatherAPI.CallOpenWeatherAPI(zipCode);
+
+} while (userInputZC == false);
